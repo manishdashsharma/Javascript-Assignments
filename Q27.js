fedb52bbdb36cc6c3f6bd6d1c2a0704a72a88209
Manishdowellresearch <manish@dowellresearch.in>
2023-01-18 02:56:56 +0530

    Q27 completed

--- Q27.js
+++ Q27.js
@@ -1,4 +1,23 @@
 // Write a program to print all the prime number between 0 to 100 (0 and 100 included)
 
+let primeNumber = [];
+let notPrimeNumber = [];
+let isPrime = true;
 
-// pending
+
+for (let i = 0; i<= 100; i++){
+    if (i > 1) {
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                notPrimeNumber.push(i);
+            }else {
+                primeNumber.push(i);
+            }
+            break
+        }
+        
+    }
+}
+
+console.log(primeNumber);
+console.log(notPrimeNumber);
